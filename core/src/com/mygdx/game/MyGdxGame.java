3ec0d637a7c20295c17a9047c2661c2d596706a4
Helena Jovanovic <helenajovanovichj21@gmail.com>
2018-03-05 20:23:02 +0100

    Treci apdejt

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,11 +1,14 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
@@ -24,30 +27,33 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	private OrthographicCamera cam;
 	private Viewport viewport;
 
-	private Texture slikaZivoti;
-	private Sprite zivoti;
-	private Sprite zivoti2;
-	private Sprite zivoti3;
+
 
 	private float njuskaX=-40, njuskaY=-400;
 
 	private Texture pecurkaSlika;
-	private float pecurkaY;
 	private Sprite pecurka1, pecurka2, pecurka3;
 
-	private boolean prviZivot = true, drugiZivot = true, treciZivot = true;
 
 	private boolean isMenu=true, isGame=false, isGameOver=false;
 	private Texture dugmeStart, dugmeQuit;
     private Sprite dugStart, dugQuit;
 
     private Random rand;
-    private int brzinaPadanja;
+    private int brzinaPadanja1;
+    private int brzinaPadanja2;
+    private int brzinaPadanja3;
     private Vector3 touch = new Vector3();
 
     private int prvaX, prvaY;
     private int drugaX, drugaY;
     private int trecaX, trecaY;
+
+    private Preferences prefs;
+    private int Score;
+    private int scoringNow=0;
+
+    private BitmapFont font;
 
 	@Override
 	public void create () {
@@ -56,14 +62,15 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 		pozadina = new Sprite(img);
         cam = new OrthographicCamera();
         viewport= new StretchViewport(480, 800, cam);
+        rand = new Random();
 
         ucitajDugmice();
         ucitajKaraktera();
-        ucitajZivote();
         ucitajPecurke();
 
-        rand = new Random();
-
+        prefs = Gdx.app.getPreferences("highscore");
+        Score = prefs.getInteger("score");
+        font = new BitmapFont(Gdx.files.internal("font.fnt"), Gdx.files.internal("font.png"), false);
 
         Gdx.input.setInputProcessor(this);
 
@@ -78,41 +85,38 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 
 		batch.begin();
 
-
-
         batch.draw(pozadina, -240, -400);
 
         if(isMenu){
+            isGame=false;
+            isGameOver=false;
             dugStart.draw(batch);
             dugQuit.draw(batch);
+            font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+            font.getData().setScale(0.5f);
+            font.draw(batch, Integer.toString(Score), 170, 370);
         }
 
         if(isGame) {
-            njuskaX -= 2.5 * Gdx.input.getAccelerometerX();
-            if (njuskaX < -240) {
-                njuskaX = -240;
-            } else if (njuskaX > 144) {
-                njuskaX = 144;
+            isMenu=false;
+            isGameOver=false;
+            igraJeUToku();
+        }
+        if(isGameOver){
+            isMenu=false;
+            isGame=false;
+            dugStart.draw(batch);
+            dugQuit.draw(batch);
+            if(scoringNow>Score){
+                prefs.putInteger("score", scoringNow);
+                prefs.flush();
+                Score = scoringNow;
             }
-            spriteNjuska.setPosition(njuskaX, njuskaY);
-            spriteNjuska.draw(batch);
+            font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+            font.getData().setScale(0.5f);
+            font.draw(batch, Integer.toString(Score), 170, 370);
 
-
-
-            pecurka1.draw(batch);
-            pecurka2.draw(batch);
-            pecurka3.draw(batch);
-
-
-            if (prviZivot) {
-                zivoti.draw(batch);
-            }
-            if (drugiZivot) {
-                zivoti2.draw(batch);
-            }
-            if (treciZivot) {
-                zivoti3.draw(batch);
-            }
+            font.draw(batch, "GAME OVER", -100, -150);
         }
 
 		batch.end();
@@ -122,7 +126,6 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
-		slikaZivoti.dispose();
 		njuska.dispose();
 		pecurkaSlika.dispose();
 	}
@@ -151,10 +154,25 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         cam.unproject(touch.set(screenX, screenY, 0));
 
-        if(touch.x >= -73 && touch.x <= 72 && touch.y > 150 && touch.y < 199){
-            isMenu=false;
-            isGame=true;
+        if(touch.x >= -73 && touch.x <= 72 && touch.y > 150 && touch.y < 199 && (isMenu==true || isGameOver==true)){
+            if(isGameOver){
+                isGameOver=false;
+                isGame=true;
+                isMenu=false;
+                prvaY=300;
+                drugaY=300;
+                trecaY=300;
+                scoringNow=0;
+            }
+            if(isMenu){
+                isMenu=false;
+                isGame=true;
+                isGameOver=false;
+            }
+
         }
+
+
         if(touch.x >= -65 && touch.x <= 66 && touch.y >= -25 && touch.y <= 34){
             Gdx.app.exit();
         }
@@ -182,7 +200,7 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
     }
 
 
-    public void ucitajDugmice(){
+    private void ucitajDugmice(){
         //Dugmici
         dugmeQuit = new Texture(Gdx.files.internal("button_quit (1).png"));
         dugmeStart = new Texture(Gdx.files.internal("button_start (1).png"));
@@ -195,7 +213,7 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
 
     }
 
-    public void ucitajKaraktera(){
+    private void ucitajKaraktera(){
         njuska = new Texture(Gdx.files.internal("njuska.png"));
 
         spriteNjuska = new Sprite(njuska);
@@ -203,28 +221,13 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
         spriteNjuska.setPosition(njuskaX, njuskaY);
     }
 
-    public void ucitajZivote(){
 
-        slikaZivoti = new Texture(Gdx.files.internal("thmb.png"));
-
-        zivoti = new Sprite(slikaZivoti);
-        zivoti.setPosition(180, 330);
-        zivoti.setSize(50, 50);
-
-        zivoti2 = new Sprite(slikaZivoti);
-        zivoti2.setPosition(125, 330);
-        zivoti2.setSize(50, 50);
-
-        zivoti3 = new Sprite(slikaZivoti);
-        zivoti3.setPosition(70, 330);
-        zivoti3.setSize(50, 50);
-    }
-    public void ucitajPecurke(){
+    private void ucitajPecurke(){
 
         pecurkaSlika = new Texture(Gdx.files.internal("c.png"));
 
         pecurka1 = new Sprite(pecurkaSlika);
-        prvaX = -200; drugaX = 300;
+        prvaX = -200; prvaY = 300;
         pecurka1.setPosition(prvaX, prvaY);
         pecurka1.setSize(100, 100);
 
@@ -237,5 +240,85 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
         trecaX = 100; trecaY = 300;
         pecurka3.setPosition(trecaX, trecaY);
         pecurka3.setSize(100, 100);
+
+        brzinaPadanja1 = rand.nextInt(3)+2;
+        brzinaPadanja2 = rand.nextInt(3)+2;
+        brzinaPadanja3 = rand.nextInt(3)+2;
+        while((brzinaPadanja1==brzinaPadanja2) || (brzinaPadanja1==brzinaPadanja3) || (brzinaPadanja2==brzinaPadanja3)){
+            brzinaPadanja1 = rand.nextInt(3)+2;
+            brzinaPadanja2 = rand.nextInt(3)+2;
+            brzinaPadanja3 = rand.nextInt(3)+2;
+        }
+    }
+
+    private void igraJeUToku(){
+        njuskaX -= 2.5 * Gdx.input.getAccelerometerX();
+        if (njuskaX < -240) {
+            njuskaX = -240;
+        } else if (njuskaX > 144) {
+            njuskaX = 144;
+        }
+        spriteNjuska.setPosition(njuskaX, njuskaY);
+        spriteNjuska.draw(batch);
+
+        font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        font.getData().setScale(0.5f);
+
+
+        pecurka1.setPosition(prvaX, prvaY);
+        pecurka2.setPosition(drugaX, drugaY);
+        pecurka3.setPosition(trecaX, trecaY);
+
+        prvaY-=brzinaPadanja1;
+        drugaY-=brzinaPadanja2;
+        trecaY-=brzinaPadanja3;
+
+        sudar();
+        GameOver();
+
+        pecurka1.draw(batch);
+        pecurka2.draw(batch);
+        pecurka3.draw(batch);
+
+
+        font.draw(batch, Integer.toString(scoringNow), -190, 370);
+
+    }
+
+    private void GameOver(){
+        if (prvaY<-400 || drugaY<-400 || trecaY<-400){
+            isGameOver=true;
+            isMenu=false;
+            isGame=false;
+        }
+    }
+
+    private void sudar(){
+        if(prvaY<-302 && njuskaX<prvaX+50 && njuskaX+96>prvaX){
+            scoringNow++;
+            prvaY=300;
+            brzinaPadanja1 = rand.nextInt(3)+2;
+            while(brzinaPadanja1==brzinaPadanja3 || brzinaPadanja1==brzinaPadanja2){
+                brzinaPadanja1 = rand.nextInt(3)+2;
+            }
+        }
+
+        if(drugaY<-302 && njuskaX<drugaX+50 && njuskaX+96>drugaX){
+            scoringNow++;
+            drugaY=300;
+            brzinaPadanja2 = rand.nextInt(3)+2;
+            while(brzinaPadanja2==brzinaPadanja1 || brzinaPadanja2==brzinaPadanja3){
+                brzinaPadanja2 = rand.nextInt(3)+2;
+            }
+        }
+
+        if(trecaY<-302 && njuskaX<trecaX+50 && njuskaX+96>trecaX){
+            scoringNow++;
+            trecaY=300;
+            brzinaPadanja3 = rand.nextInt(3)+2;
+            while(brzinaPadanja3==brzinaPadanja2 || brzinaPadanja3==brzinaPadanja1){
+                brzinaPadanja3 = rand.nextInt(3)+2;
+            }
+        }
     }
 }
